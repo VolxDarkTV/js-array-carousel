e646286ba920656f5e494a2d926e68dffa5e1eee
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-11 15:18:37 +0100

    create HTMLcollection w js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// creo i miei elementi all'interno di 'items-container'
+let container = document.getElementsByClassName('items-container');
+container = document.createElement('div').classList.add('item');
+let imageContainer = document.getElementsByClassName('item');
+
+// let imagesSource = document.createElement('img');
+console.log(container);
+console.log(imageContainer);
